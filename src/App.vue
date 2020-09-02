24920c58aaa5ffa6acee1231ac93675f77766b2d
jona737a <49908723+jona737a@users.noreply.github.com>
2020-09-02 10:58:49 +0200

    header in progress

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <v-app>
+    <v-row color="primary" height="6">
+      <v-col cols="2" offset="5">
+        <v-img src="https://www.riskofrain.com/wp-content/uploads/2019/04/ror2_logo_512-1.png" alt="logo"></v-img>
+      </v-col>
+      <v-col cols="1" offset="3">
+        <p>admin</p>
+      </v-col>
+    </v-row>
     <router-view></router-view>
   </v-app>
 </template>
