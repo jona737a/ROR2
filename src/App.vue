1ccdfe4057071e091433332a862f202f534b83b9
jona737a <49908723+jona737a@users.noreply.github.com>
2020-09-02 12:24:29 +0200

    Header + bg

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <v-app>
     <v-container fluid>
-      <v-row class="primary">
-        <v-col cols="1" class="secondary" no-gutter>
-          <p>admin</p>
+     <v-row class="header primary">
+        <v-col cols="1" no-gutter>
+          <v-icon color="text">lock</v-icon>
+          <p class="admin">Admin</p>
         </v-col>
         <v-col cols="2" offset="4">
           <v-img src="https://www.riskofrain.com/wp-content/uploads/2019/04/ror2_logo_512-1.png" alt="logo"></v-img>
         </v-col>
-        
       </v-row>
+       <v-img class="bg" height="100vh" fluid src="https://images.gamewatcherstatic.com/image/file/3/af/99013/ss_725d301ba2651921825d23f714a0c103877e03b3.1920x1080.jpg"></v-img>
       <router-view></router-view>
-      
     </v-container>
   </v-app>
 </template>
@@ -23,12 +23,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "./scss/variables.scss";
+
+
+.bg{
+  position: absolute;
+  top: 0;
+  z-index: 0;
+}
+
+.header{
+  z-index: 1;
+  position: relative;
+  opacity: 80%;
+}
+
 .container{
-  padding-top: 0;
+  padding: 0;
 }
 
 
 
+.admin{
+  color: map-get(map-get($colorz, blue), text );
+}
 
   
 </style>
