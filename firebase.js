6c09a1bc83b72ec01575f99dd048d53e2d4e27eb
jona737a <jonathansmadsen@gmail.com>
2020-09-16 12:11:06 +0200

    Firebase setup

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCqkwqsNUNrBP3Iswj52TG9ppsYW8DHI10",
+    authDomain: "ror2-eb349.firebaseapp.com",
+    databaseURL: "https://ror2-eb349.firebaseio.com",
+    projectId: "ror2-eb349",
+    storageBucket: "ror2-eb349.appspot.com",
+    messagingSenderId: "928088216643",
+    appId: "1:928088216643:web:2c00799188c8b427a1f0f6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+export const dbMenuAdd = db.collection('menuItems');
